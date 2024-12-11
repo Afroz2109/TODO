4d5fe35b05ec66ede28490ba88c2fd3c1f5cfcb2
Afroz2109 <affu962k@gmail.com>
2024-12-11 10:59:26 +0530

    itemsContainer.jsx

--- src/Components/itemsContainer.jsx
+++ src/Components/itemsContainer.jsx
@@ -6,7 +6,7 @@ export default function ItemsContainer({ todos, deleteTodo }) {
       <ul>
         {todos.map((item, index) => {
           return (
-            <li style={{color:'white'}} key={index}>
+            <li style={{color:'black'}} key={index}>
               {item.text} 
               <button
               style={{border:'none',borderRadius:'8px', backgroundColor:'red',padding:'0.3rem',marginLeft:'10px',color:'white'}} onClick={() => deleteTodo(item)}>
